aff34c88e741d5b84242a55f0e5bb49cab05116b
quinndecker <quinndecker@gmail.com>
2022-05-05 17:03:25 -0500

    postgresql added

--- harc/settings.py
+++ harc/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'harc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'harcdb',
+        'USER': 'Harc_Deckers',
+        'PASSWORD': 'Luckycharms13!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
